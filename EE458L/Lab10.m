37afb23c728918eefbede313f2d18d878434fca2
Makva08 <91294252+Makva08@users.noreply.github.com>
2022-04-17 22:51:14 +0400

    Update Lab10.m

--- EE458L/Lab10.m
+++ EE458L/Lab10.m
@@ -1,7 +1,7 @@
 clc, clear, close all;
 
 filename = 'Recording.wav';
-[x,Fs] = audioread('Recording.wav'); %read audio file
+[x,Fs] = audioread('Recording.wav'); %read audio file (HERE you can use any recording)
 %sound(x,fs); %uncomment if you want to hear signal
 A = max(x);
 V = A*2*pi*800/Fs;
@@ -46,6 +46,8 @@ title("ORIGINAL Audio Signal in FD");
 subplot(2,1,2);plot(abs(fftshift(fft(z))));
 title("Reconstructed Audio Signal in FD");
 
+
+%To use this, install DSP extension in MATLAB
 function [h, Fnyq] = LPF2(N,Fp,Fs)
 Fnyq = Fs/2;
 h = firceqrip(N,Fp/Fnyq,[0.01 0.01],'passedge');
