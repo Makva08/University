14e25fd4d63d503d694fef7573e7cd66278bfc67
Makva08 <91294252+Makva08@users.noreply.github.com>
2022-04-18 12:22:29 +0400

    Add files via upload

---
+++ Assembly x86/C-to-x86.h
@@ -0,0 +1,46 @@
+C function:
+
+int countOnes(int number) 
+{ 
+     int a ; 
+     int count = 0; 
+     int i; 
+     a = 1; 
+     for (i = 0; i < 31; i++) 
+     { 
+          if (a & number) != 0) count++; 
+          a << 1; 
+     } 
+     return(count); 
+} 
+
+Equivalent assembly language function:
+
+.global _countOnes
+_countOnes:
+	pushl %ebp
+	movl %esp,%ebp
+
+	movl 8(%ebp),%ebx    
+      xorl %eax, %eax    
+      movl $1, %ecx     
+      xorl %esi, %esi    
+      cmpl $32,%esi    
+      jge End
+For_loop:
+      movl %ebx, %edx   
+      andl %ecx, %edx   
+      cmpl $0, %edx
+      je L1
+      incl %eax
+  L1: shll $1, %ecx 
+      
+      incl %esi      
+      cmpl $32,%esi   
+      jge End
+      jmp For_loop
+
+End: movl %ebp, %esp
+      popl %ebp
+      ret
+
